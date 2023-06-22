e23ea3f63a0a05ba423ab1da78ef4e67366c2ee6
vlados096zver <81378975+vlados096zver@users.noreply.github.com>
2023-06-22 23:31:25 +0300

    up

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -34,7 +34,6 @@ export class TableComponent {
           console.log(err)
         }
       })
-
   }
 
   search(terms: Observable<string>) {
--- src/app/service/requests.service.ts
+++ src/app/service/requests.service.ts
@@ -25,42 +25,17 @@ export class RequestsService {
     return this.http.get('https://gorest.co.in/public/v2/posts');
   }
 
-  // public getSearch(value: string):Observable<any>{
-  //   return this.http.get(`https://gorest.co.in/public/v2/users/${value}`);
-  // }
-
-  // public Sorting(value: string):Observable<any>{
-  //   return this.http.get(`https://gorest.co.in/public/v2/users/${value}`);
-  // }
-
-  // public Sorting(
-  //   filter: string,
-  //   sort: string,
-  //   order: SortDirection,
-  //   page: number,
-  //   perpage: number
-  // ): Observable<any> {
-  //   const href = 'https://api.github.com/search/issues';
-  //   const requestUrl = `${href}?q=${filter}+in:title+repo:angular/components&sort=${sort}&order=${order}&page=${page}&per_page=${perpage}`;
-  //
-  //   return this.http.get<any>(requestUrl);
-  // }
-
-
-
-  getSearch(term:any) {
+  public getSearch(term:any) {
     const baseUrl: string = 'https://api.cdnjs.com/libraries';
-    const  queryUrl: string = '?search=';
+    const queryUrl: string = '?search=';
     return this.http.get(baseUrl  + queryUrl + term);
   }
 
    public getSort(
-
        sort: any,
        order: any,
        page: number,
        perpage: number
-
    ): Observable<any> {
      const href = 'https://api.github.com/search/issues';
      const requestUrl = `${href}?q=repo:angular/components&sort=${sort}&order=${order}&page=${page}&per_page=${perpage}`;
